02dac458153d02eee691327675ddb13f97e88043
Idris Akintobi Adejare <akintobiidris@gmail.com>
2024-10-16 14:18:25 +0100

    updated cors settings

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, { cors: true });
+    const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe({ transform: true, validateCustomDecorators: true }));
     const configService = app.get(ConfigService);
     const port = configService.get('PORT');
+
+    //CORS setting
+    app.enableCors({
+        origin: '*',
+        methods: 'GET,POST',
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+    });
+
     await app.listen(port);
 }
 bootstrap();
